b3ee32436ae5b1b055311ea00e204086d5e61b52
simranpirthiani <22020586@lums.edu.pk>
2020-06-20 15:35:14 -0400

    Commiting code for cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { if (ncol(x)==nrow(x) && det(x)!=0) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinverse <- function() m <<- inv(x)
+  getinverse<-function() m
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+  
+}else{
+  return(message("The matrix is'n invertible."))
+}
 
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m <- inv(data, ...)
+  x$setinverse(m)
+  m
 }
